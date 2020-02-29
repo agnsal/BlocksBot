7405f2f93e9fd0e3962b8904a58d95146ffc9e4a
agnsal <agneses92@hotmail.it>
2020-02-29 20:06:04 +0100

    Datalog Engine feature added.

--- DecisionMakerAgent.py
+++ DecisionMakerAgent.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and limitations 
 '''
 
 import ast
-import collections
 from pyDatalog import pyDatalog
 
 from RedisManager import RedisManager
@@ -24,7 +23,7 @@ RedisConfig = Yamler.getConfigDict("Configs/RedisConfig.yaml")
 DMAConfig = Yamler.getConfigDict("Configs/DMAConfig.yaml")
 
 
-def getAverageResultFromRedisQueue(redis, queue, emotions):
+def getAverageEmotionsFromRedisQueue(redis, queue, emotions):
     assert isinstance(queue, str)
     assert isinstance(emotions, list)
     avg = {}
@@ -54,6 +53,26 @@ def getAverageResultFromRedisQueue(redis, queue, emotions):
                 'fear': fearSum/resN, 'disgust': disgustSum/resN}
     return avg
 
+def getAverageAttitudeFromRedisQueue(redis, queue):
+    assert isinstance(queue, str)
+    avg = {}
+    resList = []
+    print(queue + ": " + str(redis.getRedisQueueLen(queue)))
+    while redis.getRedisQueueLen(queue) > 0:
+        elem = redis.lPopFromRedisQueue(queue)
+        if isinstance(elem, bytes):
+            elem = elem.decode()
+        resList.append(ast.literal_eval(elem))
+    print(resList)  # Test
+    attitude = 0
+    resN = len(resList)
+    if resN != 0:
+        for elem in resList:
+            if isinstance(elem, float) or isinstance(elem, int):
+                attitude += elem
+        avg = attitude / resN
+    return avg
+
 
 def facialVocalCompare(facialRes, vocalRes, emotions, facialW=2, vocalW=1):
     assert isinstance(facialRes, dict)
@@ -73,6 +92,49 @@ def facialVocalCompare(facialRes, vocalRes, emotions, facialW=2, vocalW=1):
         res = vocalRes
     return res
 
+def learn():
+    pyDatalog.create_terms('positive, neutral, negative, firstEmo, secondEmo, poseAttitude, X, Y, A, takeDecision, D, H, I, K')
+    print("Learning started...")
+    pyDatalog.assert_fact('positive', 'happiness')
+    pyDatalog.assert_fact('neutral', 'neutral')
+    pyDatalog.assert_fact('negative', 'sadness')
+    pyDatalog.assert_fact('negative', 'fear')
+    pyDatalog.assert_fact('negative', 'angry')
+
+    pyDatalog.load("""
+    firstEmo(X) <= firstAvgEmo(X)
+    secondEmo(Y) <= secondAvgEmo(Y)
+    attitude(A) <= poseAttitude(A)
+
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & neutral(Y) & neutral(A)
+    takeDecision(Y) <= firstEmo(X) & secondEmo(Y) & attitude(A) & neutral(X) & neutral(A)
+
+    # 0, +, -
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & neutral(X) & positive(Y) & negative(A)
+    # 0, -, +
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & neutral(X) & negative(Y) & positive(A)
+    # +, 0, +
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & positive(X) & neutral(Y) & positive(A)
+    # -, 0, -
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & negative(X) & neutral(Y) & negative(A)
+
+    # -, -, _ (-, -, + problem ???????????)
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & negative(X) & negative(Y) & neutral(A)
+    # +, +, _ (+, +, - problem ???????????)
+    takeDecision(X) <= firstEmo(X) & secondEmo(Y) & attitude(A) & negative(X) & negative(Y) & neutral(A)
+
+    # 0, +, +
+    takeDecision(Y) <= firstEmo(X) & secondEmo(Y) & attitude(A) & neutral(X) & positive(Y) & positive(A)
+    # 0, -, -
+    takeDecision(Y) <= firstEmo(X) & secondEmo(Y) & attitude(A) & neutral(X) & negative(Y) & negative(A)
+    # +, 0, -
+    takeDecision(Y) <= firstEmo(X) & secondEmo(Y) & attitude(A) & positive(X) & neutral(Y) & negative(A)
+    # -, 0, +
+    takeDecision(Y) <= firstEmo(X) & secondEmo(Y) & attitude(A) & negative(X) & neutral(Y) & positive(A)
+    """)
+
+    print("Learning finished")
+
 
 def main():
     r = RedisManager(host=RedisConfig['host'], port=RedisConfig['port'], db=RedisConfig['db'],
@@ -80,7 +142,7 @@ def main():
     sub = r.getRedisPubSub()
     sub.subscribe(RedisConfig['VocalChannel'])
 
-    behaviourFilePath = 'behaviour.dl'
+    learn()
 
     while True:
         newMsg = sub.get_message()
@@ -88,7 +150,7 @@ def main():
             if newMsg['type'] == 'message':
                 print("Vocal Result: " + str(newMsg))  # Test
                 vocalRes = ast.literal_eval(newMsg['data'].decode())
-                facialRes = getAverageResultFromRedisQueue(r, queue=RedisConfig['FacialQueue'],
+                facialRes = getAverageEmotionsFromRedisQueue(r, queue=RedisConfig['FacialQueue'],
                                                            emotions=DMAConfig['emotions'])
                 facialVocal = facialVocalCompare(facialRes, vocalRes, emotions=DMAConfig['emotions'])
                 print(facialVocal)  # Test
@@ -96,14 +158,27 @@ def main():
                     sortedEmotions = {k: v for k, v in sorted(facialVocal.items(), key=lambda item: item[1])}
                     print(sortedEmotions)  # Test
                     sortedEmoList = list(sortedEmotions.items())
-                    topEmotions = dict([sortedEmoList[-1], sortedEmoList[-2]])
+                    firstEmotion = sortedEmoList[-1]
+                    secondEmotion = sortedEmoList[-2]
+                    topEmotions = dict([firstEmotion, secondEmotion])
                     print(topEmotions)
+                    decision = firstEmotion
                     diff = 0
                     for k in topEmotions:
                         diff -= topEmotions[k]
                     diff = abs(diff)
                     if diff <= DMAConfig['poseTestThreshold']:
                         print(diff)  # Test
+                        attitude = getAverageAttitudeFromRedisQueue(r, queue=RedisConfig['PoseQueue'])
+                        if attitude:
+                            + firstAvgEmo(str(firstEmotion).replace("}", '').split(":")[0])
+                            + secondAvgEmo(str(secondEmotion).replace("}", '').split(":")[0])
+                            + poseAttitude(attitude)
+                            decision = str(pyDatalog.ask("takeDecision(D)"))
+                            - firstEmo(str(firstEmotion).replace("}", '').split(":")[0])
+                            - secondEmo(str(secondEmotion).replace("}", '').split(":")[0])
+                            - poseAttitude(attitude)
+                    print("Decision: " + str(decision))  # Test
 
 
 
